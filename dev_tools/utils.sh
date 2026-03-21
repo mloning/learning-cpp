13e08802e7877cdb919b94e2e6ad4fddbfafcef4
mloning <21020482+mloning@users.noreply.github.com>
2026-03-21 22:34:28 +0100

    update build cmd with stricter settings

--- dev_tools/utils.sh
+++ dev_tools/utils.sh
@@ -2,8 +2,26 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-build(){
+build() {
   rm -r build || true
   mkdir build/
-  g++ -v -std=c++17 -pedantic-errors -Wall -Weffc++ -Wextra -Wconversion -Wsign-conversion -Wshadow -Werror -ggdb "$@"
+  g++ -v -std=c++17 \
+    -pedantic-errors \
+    -Wall \
+    -Weffc++ \
+    -Wextra \
+    -Wconversion \
+    -Wsign-conversion \
+    -Wshadow \
+    -Werror \
+    -Wnon-virtual-dtor \
+    -Wold-style-cast \
+    -Wcast-align \
+    -Woverloaded-virtual \
+    -Wpointer-arith \
+    -Wundef \
+    -Wno-unused \
+    -Wno-unused-parameter \
+    -ggdb \
+    "$@"
 }
